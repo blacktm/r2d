b594c5c082c85adcd03689e6492d425b5cd7ab51
Tom Black <blacktm@gmail.com>
2013-05-04 12:00:35 -0400

    Continued implementing audio

--- lib/r2d/song.rb
+++ lib/r2d/song.rb
@@ -1,41 +1,37 @@
 # song.rb
 
 class Song
-
+  
   def initialize(path)
-    @song = Window.song(path)
+    @song = R2D::Window.song(path)
   end
-
+  
   def play
     @song.play
   end
-
+  
   def play_loop
     @song.play(true)
   end
-
+  
   def pause
     @song.pause
   end
-
+  
   def playing?
     @song.playing?
   end
-
+  
   def paused?
     @song.paused?
   end
-
-  def song
-    @song.stop
-  end
-
-  def volume
-    @song.volume
-  end
-
+  
   def stop
     @song.stop
   end
-
+  
+  def volume
+    @song.volume
+  end
+  
 end
--- lib/r2d/sound.rb
+++ lib/r2d/sound.rb
@@ -1,5 +1,37 @@
 # sound.rb
 
 class Sound
-
+  
+  def initialize(path)
+    @sound = R2D::Window.sound(path)
+  end
+  
+  def play
+    @sound.play
+  end
+  
+  def play_loop
+    @sound.play(true)
+  end
+  
+  def pause
+    @sound.pause
+  end
+  
+  def playing?
+    @sound.playing?
+  end
+  
+  def paused?
+    @sound.paused?
+  end
+  
+  def stop
+    @sound.stop
+  end
+  
+  def volume
+    @sound.volume
+  end
+  
 end
