d60d3fcb4f0886fdae2b9f59a0defd3db56b3b10
Tom Black <blacktm@gmail.com>
2013-05-04 11:59:21 -0400

    Implemented Image

--- examples/image.rb
+++ examples/image.rb
@@ -4,4 +4,36 @@ window width: 640, height: 480
 
 img = Image.new(100, 100, 'media/gem.png')
 
+pos = true
+
+key_down 'h' do
+  if pos
+    img.height += 1
+  else
+    img.height -= 1
+  end
+end
+
+key_down 'w' do
+  if pos
+    img.width += 1
+  else
+    img.width -= 1
+  end
+end
+
+on_key 's' do
+  puts "width: " + img.width.to_s
+  puts "height: " + img.height.to_s
+end
+
+
+on_key 'space' do
+  if pos
+    pos = false
+  else
+    pos = true
+  end
+end
+
 window :show
--- lib/r2d/image.rb
+++ lib/r2d/image.rb
@@ -1,10 +1,39 @@
 # image.rb
 
 class Image
-  attr_accessor :x, :y
   
-  def initialize(x, y, path)
+  attr_accessor :x, :y
+  attr_reader :width, :height, :f_x, :f_y
+  
+  def initialize(x, y, path, visible=true)
     @x, @y, @path = x, y, path
+    @f_x, @f_y = 1, 1
+    @image = R2D::Window.image(path)
+    @o_w, @o_h = @image.width, @image.height
+    @width, @height = @o_w, @o_h
+    if visible then add end
+  end
+  
+  def width=(w)
+    @f_x = w / @o_w.to_f
+    @width = w
+  end
+  
+  def height=(h)
+    @f_y = h / @o_h.to_f
+    @height = h
+  end
+  
+  def add
+    R2D::Window.add(self)
+  end
+  
+  def remove
+    R2D::Window.remove(self)
+  end
+  
+  def adapter
+    @image
   end
   
 end
