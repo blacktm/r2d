a55f86e80ca1c13a49eafc567e5a321e3f48a12b
Tom Black <blacktm@gmail.com>
2013-10-12 00:55:08 -0400

    Removing the demo

--- examples/demo.rb
+++
@@ -1,6 +0,0 @@
-
-require 'r2d'
-
-window = R2D::Window.new
-
-window.demo
--- ext/r2d/r2d.c
+++ ext/r2d/r2d.c
@@ -56,65 +56,6 @@ static VALUE r2d_draw_triangle(VALUE self,
 
 
 /*
- * Starts the R2D demo
- */
-static VALUE r2d_demo(VALUE self) {
-  
-  GLFWwindow* window;
-  
-  glfwSetErrorCallback(error_callback);
-  
-  if (!glfwInit())
-    exit(EXIT_FAILURE);
-  
-  window = glfwCreateWindow(640, 480, "R2D", NULL, NULL);
-  if (!window) {
-    glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
-  
-  glfwMakeContextCurrent(window);
-  
-  while (!glfwWindowShouldClose(window)) {
-    
-    float ratio;
-    int width, height;
-    
-    glfwGetWindowSize(window, &width, &height);
-    ratio = width / (float) height;
-    
-    glViewport(0, 0, width, height);
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-    glMatrixMode(GL_MODELVIEW);
-    
-    glLoadIdentity();
-    glRotatef((float) glfwGetTime() * 20.f, 0.f, 0.f, 1.f);
-    
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.f, 0.f, 0.f);
-    glVertex3f(-0.6f, -0.4f, 0.f);
-    glColor3f(0.f, 1.f, 0.f);
-    glVertex3f(0.6f, -0.4f, 0.f);
-    glColor3f(0.f, 0.f, 1.f);
-    glVertex3f(0.f, 0.6f, 0.f);
-    glEnd();
-    
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-  
-  glfwDestroyWindow(window);
-  
-  glfwTerminate();
-  exit(EXIT_SUCCESS);
-}
-
-
-/*
  * R2D::Window#show
  */
 static VALUE r2d_show(VALUE self) {
@@ -178,14 +119,6 @@ void Init_r2d(void) {
   /* R2D::Window */
   r2d_window_klass = rb_define_class_under(r2d_module, "Window", rb_cObject);
   
-  rb_define_method(r2d_window_klass, "demo", r2d_demo, 0);
   rb_define_method(r2d_window_klass, "show", r2d_show, 0);
   rb_define_method(r2d_window_klass, "draw_triangle", r2d_draw_triangle, 9);
 }
-
-
-
-
-
-
-
